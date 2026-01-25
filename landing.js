fa866fbdec2b3b70a3549661b75c11b387c22d00
Nithilan <Nithilan.Karthik097@gmail.com>
2026-01-24 19:35:53 -0600

    Make scroll animations more dramatic

--- landing.js
+++ landing.js
@@ -2,12 +2,13 @@ const revealElements = document.querySelectorAll(".reveal");
 const slideElements = document.querySelectorAll(".reveal-slide");
 
 const observerOptions = {
-  threshold: 0.15,
+  threshold: 0.1,
+  rootMargin: "0px 0px -100px 0px",
 };
 
 const slideObserverOptions = {
-  threshold: 0.25,
-  rootMargin: "0px 0px -50px 0px",
+  threshold: 0.15,
+  rootMargin: "0px 0px -150px 0px",
 };
 
 const observer = new IntersectionObserver((entries) => {
